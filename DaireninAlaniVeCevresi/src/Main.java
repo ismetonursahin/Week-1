c195da4074997c8d31bdb659c54ed9bd7ed21635
İsmet Onur ŞAHİN <110236136+ismetonursahin@users.noreply.github.com>
2023-12-11 09:22:03 +0300

    first commit

--- DaireninAlaniVeCevresi/src/Main.java
+++ DaireninAlaniVeCevresi/src/Main.java
@@ -5,20 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         double r,perimeter,circleArea ;
         double pi = 3.14;
         double a ;
 
+        //Kullanıcıdan yarıçap ve istenilen dilimin açı değeri alınan kısım.
         System.out.print("Yarıçap uzunluğunu giriniz : ");
         r = input.nextDouble();
-
         System.out.print("Açı Değeri Giriniz : ");
         a = input.nextDouble();
 
-        circleArea = pi * r*r;
+        circleArea = pi * r*r;  //Dairenin alanının hesaplanması
         System.out.println("Dairenin Alanı : " + circleArea);
 
-        perimeter = (pi * (r*r) * a) / 360;
+
+        perimeter = (pi * (r*r) * a) / 360; //Dairenin çevresi hesaplanması
         System.out.println("Dilimin Çevresi : " + perimeter);
+
     }
 }
