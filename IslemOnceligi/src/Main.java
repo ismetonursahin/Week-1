1c0226022adee4469fc2c5e4969231fb660b0e5c
İsmet Onur ŞAHİN <110236136+ismetonursahin@users.noreply.github.com>
2023-12-11 11:36:42 +0300

    first commit

---
+++ IslemOnceligi/src/Main.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a,b,c ;
+        int total;
+
+        System.out.print("A Sayısını Giriniz : ");
+        a = input.nextInt();
+        System.out.print("B Sayısını Giriniz : ");
+        b = input.nextInt();
+        System.out.print("C Sayısını Giriniz : ");
+        c = input.nextInt();
+
+        total  = a+b*c-b;
+        System.out.println(total);
+
+    }
+}
