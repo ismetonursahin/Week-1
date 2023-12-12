ba2719558fbd02b9708b82958a94e20ec3068221
İsmet Onur ŞAHİN <110236136+ismetonursahin@users.noreply.github.com>
2023-12-12 14:18:48 +0300

    first commit

--- Asal_Sayi/src/Main.java
+++ Asal_Sayi/src/Main.java
@@ -1,16 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        for(int i = 1 ; i <= 100 ; i++) {
-              for (int j = 2 ; j <= i ; j++){
-                  if(i % j == 0){
-                      System.out.println(j +"asal değil");
-                  }else {
-                      System.out.println("asal");
-                  }
-              }
+        for (int i = 2; i <= 100; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(i + " is prime");
+            }
         }
     }
 }
